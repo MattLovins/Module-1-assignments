91c0157c3ff4fb4b71e255f92c10f48c33189f4b
MattLovins <mlovins3@ivytech.edu>
2025-09-05 11:31:17 -0400

    Add files via upload

---
+++ itsRainingCatsAndDogs.py
@@ -0,0 +1,8 @@
+#Initialization
+strCats = "cats"
+strDogs = "dogs"
+strFinalStatement = ""
+#process data
+strFinalStatement = f"It is raining {strCats} and {strDogs}."
+#output information
+print(strFinalStatement)
